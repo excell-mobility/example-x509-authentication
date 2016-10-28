aefdcaeb9a184b5ca5c827cd808f6b2505c17658
Andre Rademacher <andre.rademacher.business@googlemail.com>
2016-10-28 17:07:12 +0000

    added certification creation scripts

---
+++ ZendExpressiveRestX509TestApplication/bin/client/createKeyPairAndCertificateSigningRequest.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# settings key pair
+clientKeyPairAlgorithm="-aes256"
+clientKeyPairBits=4096
+clientKeyPairPath="./../../data/client/"
+
+clientPrivateKeyFileName="client.private.encrypted.key"
+clientPublicKeyFileName="client.public.pem"
+
+clientCsrFileName="client.csr"
+clientCsrAlgorithm="-sha512"
+
+# step 1: create the client's private key
+mkdir -p ${clientKeyPairPath}
+openssl genrsa ${clientKeyPairAlgorithm} -out ${clientKeyPairPath}${clientPrivateKeyFileName} ${clientKeyPairBits}
+chmod 400 ${clientKeyPairPath}${clientPrivateKeyFileName}
+
+# step 2: calculate the client's public key
+openssl rsa -in ${clientKeyPairPath}${clientPrivateKeyFileName} -pubout -out ${clientKeyPairPath}${clientPublicKeyFileName}
+chmod 444 ${clientKeyPairPath}${clientPublicKeyFileName}
+
+# step 3: create the CSR (certificate signing request)
+openssl req -new ${clientCsrAlgorithm} -key ${clientKeyPairPath}${clientPrivateKeyFileName} -out ${clientKeyPairPath}${clientCsrFileName}
+chmod 444 ${clientKeyPairPath}${clientCsrFileName}
