e8fde05b35a1c3ee813846936bfa157151a2edad
Andre Rademacher <andre.rademacher.business@googlemail.com>
2016-11-10 15:13:09 +0000

    with Bodyparams for JSON parsing and simple parameter Validation

--- ZendExpressiveRestX509TestApplication/config/autoload/routes.global.php
+++ ZendExpressiveRestX509TestApplication/config/autoload/routes.global.php
@@ -1,9 +1,18 @@
 <?php
 
+use Zend\Expressive\Helper\BodyParams\BodyParamsMiddleware;
+
 return [
     'dependencies' => [
         'invokables' => [
+
+            // Zend specific stuff
             Zend\Expressive\Router\RouterInterface::class => Zend\Expressive\Router\FastRouteRouter::class,
+
+            // helper
+            BodyParamsMiddleware::class => BodyParamsMiddleware::class,
+
+            // actions
             App\PingAction::class => App\PingAction::class,
         ],
         'factories' => [
@@ -20,8 +29,11 @@ return [
         ],
         [
             'name' => 'api.ping',
-            'path' => '/hello',
-            'middleware' => App\PingAction::class,
+            'path' => '/api/v1.0/hello',
+            'middleware' => [
+                BodyParamsMiddleware::class,
+                App\PingAction::class,
+            ],
             'allowed_methods' => ['GET', 'POST'],
         ],
     ],
--- ZendExpressiveRestX509TestApplication/src/PingAction.php
+++ ZendExpressiveRestX509TestApplication/src/PingAction.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Zend\Diactoros\Response;
 use Zend\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -10,6 +11,14 @@ class PingAction
 {
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null)
     {
+        $requestParams = $request->getParsedBody();
+
+        // check for "name" parameter
+        if (!isset($requestParams['name'])) {
+            $errorResponse = new Response\TextResponse('Bad request: "name" parameter is missing!', 400);
+            return $errorResponse;
+        }
+
         return new JsonResponse(['ack' => time()]);
     }
 }
