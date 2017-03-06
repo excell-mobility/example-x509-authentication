692ca85eb897eae9773fcdf727d8f6c773639ea9
Andre Rademacher <andre.rademacher.business@googlemail.com>
2017-03-06 15:35:57 +0000

    chapter 3 fixes

--- ZendExpressiveRestX509TestApplication/bin/client/test.php
+++ ZendExpressiveRestX509TestApplication/bin/client/test.php
@@ -1,27 +1,28 @@
+#!/usr/bin/php
 <?php
  
-$url = "https://example-x509-authentication.dev";
-$cert_file = './example.org.pem';
+$targetUrl = "https://il-test.excell-mobility.de/api/v1/health";
+$cert_file = './exampleFiles/example.org.pem';
 $cert_password = 'example123';
  
 $ch = curl_init();
  
-$options = array( 
+$options = array(
+    CURLOPT_URL => $targetUrl ,
     CURLOPT_RETURNTRANSFER => true,
-    //CURLOPT_HEADER         => true,
     CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_SSL_VERIFYHOST => false,
-    CURLOPT_SSL_VERIFYPEER => false,
-     
     CURLOPT_USERAGENT => 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)',
-    //CURLOPT_VERBOSE        => true,
-    CURLOPT_URL => $url ,
-    CURLOPT_SSLCERT => $cert_file ,
-    CURLOPT_SSLCERTPASSWD => $cert_password ,
+
+    CURLOPT_SSLCERT => $cert_file,              // remove this line for HTTP/1.1 401 Unauthorized -> "SSL client certificate not sent"
+    CURLOPT_SSLCERTPASSWD => $cert_password,
+    CURLOPT_SSL_VERIFYHOST => 2,
+    CURLOPT_SSL_VERIFYPEER => true,
+
+    // for demo & debugging purpose only!
+    CURLOPT_VERBOSE        => true,
 );
  
 curl_setopt_array($ch , $options);
- 
 $output = curl_exec($ch);
  
 if(!$output)
@@ -30,5 +31,5 @@ if(!$output)
 }
 else
 {
-    echo htmlentities($output);
+    echo $output . PHP_EOL;
 }
